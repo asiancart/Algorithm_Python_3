d40b84dbbfe5c1b89a0072693eafd49fe1e4038f
asiancart <asiancart@hotmail.com>
2022-05-23 14:21:38 +0300

    clickbait headline generator

--- main.py
+++ main.py
@@ -1,70 +1,125 @@
-import random,sys
+import random
 
-JAPANESE_NUMBERS =  {1: 'ICHI', 2: 'NI', 3: 'SAN',4: 'SHI', 5: 'GO', 6: 'ROKU'}
-
-print('''Cho-han, In this traditional Japanese dice game, two dice are rolled in a bamboo cup by the dealer sitting on the floor. The player must guess if the dice total to an even (cho) or odd (han) number. ''')
-
-purse = 5000
-
-while True:
-    print('You have', purse, 'mon. How much do you bet? (or QUIT)')
-    while True:
-        pot = input('> ')
-        if pot.upper() == 'QUIT':
-            print('Thanks for playing.')
-            sys.exit()
-        elif not pot.isdecimal():
-            print('Please enter a number. ')
-        elif int(pot) > purse :
-            print('You do not have enough to make that bet.')
-        else:
-            pot = int(pot)
-            break
+OBJECT_PRONOUNS = ['Her', 'Him', 'Them']
+POSSESIVE_PRONOUNS = ['Her', 'His', 'Their']
+PERSONAL_PRONOUNS = ['She', 'He', 'They']
+STATES = ['California', 'Texas', 'Florida', 'New York', 'Pennsylvania','Illinois', 'Ohio', 'Georgia', 'North Carolina', 'Michigan']
+NOUNS = ['Athlete', 'Clown', 'Shovel', 'Paleo Diet', 'Doctor', 'Parent','Cat', 'Dog', 'Chicken', 'Robot', 'Video Game', 'Avocado','Plastic Straw','Serial Killer', 'Telephone Psychic']
+PLACES = ['House', 'Attic', 'Bank Deposit Box', 'School', 'Basement','Workplace', 'Donut Shop', 'Apocalypse Bunker']
+WHEN = ['Soon', 'This Year', 'Later Today', 'RIGHT NOW', 'Next Week']
 
 
-    dice1 = random.randint(1,6)
-    dice2 = random.randint(1,6)
-
-    print('The dealer swirls the cup and you hear the rattle of dice.')
-    print('The dealer slams the cup on the floor, still covering the')
-    print('dice and asks for your bet.')
+def main():
+    print('Clickbait Headline Generator')
     print()
-    print(' CHO (even) or HAN (odd)?')
 
-
+    print('Our website needs to trick people into looking at ads!')
     while True:
-        bet = input('> ').upper()
-        if bet != 'CHO' and bet != 'HAN':
-            print('please enter either CHO or HAN. ')
-            continue
+        print('Enter the number of clickbait headlines to generate:')
+        response = input('> ')
+        if not response.isdecimal():
+            print('Please enter a number.')
         else:
+            numberOfHeadlines = int(response)
             break
 
-    print('The dealer lifts the cup to reveal:')
-    print(' ', JAPANESE_NUMBERS[dice1], '-', JAPANESE_NUMBERS[dice2])
-    print(' ', dice1, '-', dice2)
+
+    for i in range(numberOfHeadlines):
+        clickbaitType = random.randint(1,8)
+
+        if clickbaitType == 1:
+            headline = generateAreMillennialsKillingHeadline()
+        elif clickbaitType == 2:
+            headline = generateWhatYouDontKnowHeadline()
+        elif clickbaitType == 3:
+            headline = generateBigCompaniesHateHerHeadline()
+        elif clickbaitType == 4:
+            headline = generateYouWontBelieveHeadline()
+        elif clickbaitType == 5:
+            headline = generateDontWantYouToKnowHeadline()
+        elif clickbaitType == 6:
+            headline = generateGiftIdeaHeadline()
+        elif clickbaitType == 7:
+            headline = generateReasonsWhyHeadline()
+        elif clickbaitType == 8:
+            headline = generateJobAutomatedHeadline()
 
 
-    rollIsEven = (dice1+dice2) % 2 == 0
-    if rollIsEven:
-        correctBet = 'CHO'
+        print(headline)
+    print()
+
+    website = random.choice(['wobsite', 'blag', 'Facebuuk', 'Googles','Facesbook', 'Tweedie', 'Pastagram'])
+
+    when = random.choice(WHEN).lower()
+    print('Post these to our', website, when, 'or you\'re fired!')
+
+
+def generateAreMillennialsKillingHeadline():
+    noun = random.choice(NOUNS)
+    return 'Are Millennials Killing {} Industry ?'.format(noun)
+
+
+def generateWhatYouDontKnowHeadline():
+    noun = random.choice(NOUNS)
+    pluralNoun = random.choice(NOUNS) + 's'
+    when = random.choice(WHEN)
+    return 'Without This {}, {} Could Kill You {}'.format(noun, pluralNoun, when)
+
+
+def generateBigCompaniesHateHerHeadline():
+    pronoun = random.choice(OBJECT_PRONOUNS)
+    state = random.choice(STATES)
+    noun1 = random.choice(NOUNS)
+    noun2 = random.choice(NOUNS)
+    return 'Big Companies Hate {}! See How This {} {} Invented a Cheaper {}'.format(pronoun, state, noun1, noun2)
+
+
+def generateYouWontBelieveHeadline():
+     state = random.choice(STATES)
+     noun = random.choice(NOUNS)
+     pronoun = random.choice(POSSESIVE_PRONOUNS)
+     place = random.choice(PLACES)
+     return 'You Won\'t Believe What This {} {} Found in {} {}'.format(state, noun,pronoun, place)
+
+
+def generateDontWantYouToKnowHeadline():
+    pluralNoun1 = random.choice(NOUNS) + 's'
+    pluralNoun2 = random.choice(NOUNS) + 's'
+    return 'What {} Don\'t Want You To Know About {}'.format(pluralNoun1, pluralNoun2)
+
+
+def generateGiftIdeaHeadline():
+     number = random.randint(7, 15)
+     noun = random.choice(NOUNS)
+     state = random.choice(STATES)
+     return '{} Gift Ideas to Give Your {} From {}'.format(number, noun, state)
+
+
+def generateReasonsWhyHeadline():
+    number1 = random.randint(3, 19)
+    pluralNoun = random.choice(NOUNS) + 's'
+    number2 = random.randint(1, number1)
+    return '{} Reasons Why {} Are More Interesting Than You Think (Number {} Will Surprise You!)'.format(number1, pluralNoun, number2)
+
+
+def generateJobAutomatedHeadline():
+    state = random.choice(STATES)
+    noun = random.choice(NOUNS)
+
+    i = random.randint(0,2)
+    pronoun1 = POSSESIVE_PRONOUNS[i]
+    pronoun2 = PERSONAL_PRONOUNS[i]
+    if pronoun1 == 'Their':
+        return 'This {} {} Didn\'t Think Robots Would Take {} Job. {} Were Wrong.'.format(state, noun, pronoun1, pronoun2)
     else:
-        correctBet = 'HAN'
+        return 'This {} {} Didn\'t Think Robots Would Take {} Job. {} Was Wrong.'.format(state, noun, pronoun1, pronoun2)
 
 
-    playerWon = bet == correctBet
+
+if __name__ == '__main__':
+    main()
 
 
-    if playerWon:
-        print('You won! You take', pot, 'mon.')
-        purse = purse + pot
-        print('The house collects a', pot // 10, 'mon fee.')
-        purse = purse - (pot//10)
-    else:
-        purse= purse - pot
-        print('You lost .')
 
-    if purse == 0:
-        print('You have run out of money!')
-        print('Thanks for playing!')
-        sys.exit()
+
+
