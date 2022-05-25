884668b30c97fb987a49b8c22933ad4046db5eca
asiancart <asiancart@hotmail.com>
2022-05-25 17:26:24 +0300

    Conway's Game of Life

--- main.py
+++ main.py
@@ -1,41 +1,70 @@
-def main():
-    print('diamonds drawing program')
+import copy , random , sys ,time
 
-    for diamondSize in range(0,6):
-        displayOutlineDiamond(diamondSize)
+WIDTH = 79
+HEIGHT = 20
+
+ALIVE = '0'
+DEAD = ' '
+
+nextCells = {}
+
+for x in range(WIDTH):
+    for y in range(HEIGHT):
+        if random.randint(0,1) == 0:
+            nextCells[(x,y)] = ALIVE
+        else:
+            nextCells[(x,y)] = DEAD
+
+
+while True:
+    print('\n' * 50)
+    cells = copy.deepcopy(nextCells)
+
+    for y in range(HEIGHT):
+        for x in range(WIDTH):
+            print(cells[(x,y)],end='')
         print()
-        displayFilledDiamond(diamondSize)
-        print()
+    print('Press Ctrl-C to quit.')
 
 
-def displayOutlineDiamond(size):
-    for i in range(size):
-        print(' ' * (size - i - 1), end='')
-        print('/', end='')
-        print(' ' * (i * 2), end='')
-        print('\\')
+    for x in range(WIDTH):
+        for y in range(HEIGHT):
+            left = (x-1) % WIDTH
+            right =(x+1) % WIDTH
+            above = (y-1) % HEIGHT
+            below = (y+1) % HEIGHT
 
 
-    for i in range(size):
-        print(' '* i,end='')
-        print('\\', end='')
-        print(' ' * ((size - i - 1) * 2), end='')
-        print('/')
+            numNeighbors = 0
+            if cells[(left,above)] == ALIVE:
+                numNeighbors += 1
+            if cells[(x,above)] == ALIVE:
+                numNeighbors +=1
+            if cells[(right,above)] == ALIVE:
+                numNeighbors += 1
+            if cells[(left,y)] == ALIVE:
+                numNeighbors +=1
+            if cells[(right,y)] == ALIVE:
+                numNeighbors +=1
+            if cells[(left, below)] == ALIVE:
+                numNeighbors += 1
+            if cells[(x, below)] == ALIVE:
+                numNeighbors += 1
+            if cells[(right, below)] == ALIVE:
+                numNeighbors += 1
 
 
-def displayFilledDiamond(size):
-    for i in range(size):
-        print(' '*(size-i-1),end='')
-        print('/'*(i+1),end='')
-        print('\\'*(i+1))
+            if cells[(x,y)] == ALIVE and(numNeighbors == 2 or numNeighbors==3):
+                nextCells[(x,y)] = ALIVE
+            elif cells[(x,y)] == DEAD and numNeighbors == 3:
+                nextCells[(x,y)] = ALIVE
+            else:
+                nextCells[(x,y)] =DEAD
 
 
-    for i in range(size):
-        print(' ' * i, end='')
-        print('\\' * (size - i), end='')
-        print('/' * (size - i))
-
-
-if __name__ == '__main__':
-    main()
+    try:
+        time.sleep(1)
+    except KeyboardInterrupt:
+        print("Conway's Game of Life")
+        sys.exit()
 
