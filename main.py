bc0313e5744815e98a5589ede67103d39267fca0
asiancart <asiancart@hotmail.com>
2022-05-26 20:13:18 +0300

    FAST DRAW

--- main.py
+++ main.py
@@ -1,30 +1,34 @@
-import math,sys
+import random , sys ,time
 
+print('Fast Draw, by asiancart')
+print()
+print('Time to test your reflexes and see if you are the fastest')
+print('When you see "DRAW", you have 0.3 seconds to press Enter.')
+print('But you lose if you press Enter before "DRAW" appears.')
+print()
+input('Press Enter to begin...')
 
 while True:
-    print('Enter a positive whole number to factor (or QUIT):')
-    response = input('> ')
-    if response.upper() == 'QUIT':
+    print()
+    print('It is high noon...')
+    time.sleep(random.randint(20, 50) / 10.0)
+    print('DRAW')
+    drawTime=time.time()
+    input()
+    timeElapsed=time.time()-drawTime
+
+    if timeElapsed < 0.01:
+        print('You drew before "DRAW" appeared! You lose.')
+    elif timeElapsed > 0.3:
+        timeElapsed = round(timeElapsed, 4)
+        print('You took', timeElapsed, 'seconds to draw. Too slow!')
+    else:
+        timeElapsed = round(timeElapsed, 4)
+        print('You took', timeElapsed, 'seconds to draw.')
+        print('You are the fastest draw in the west! You win!')
+
+    print('Enter QUIT to stop, or press Enter to play again.')
+    response = input('> ').upper()
+    if response == 'QUIT':
+        print('Thanks for playing!')
         sys.exit()
-
-
-    if not (response.isdecimal() and int(response) > 0):
-        continue
-
-    number=int(response)
-
-    factors=[]
-
-    for i in range(1,int(math.sqrt(number))+1):
-        if number % i ==0:
-            factors.append(i)
-            factors.append(number//i)
-
-
-    factors = list(set(factors))
-    factors.sort()
-
-    for i,factor in enumerate(factors):
-        factors[i]=str(factor)
-    print(', '.join(factors))
-
