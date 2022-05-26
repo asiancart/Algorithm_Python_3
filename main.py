9212042fb4b74131a48265c8d14e96b0ca9a3705
asiancart <asiancart@hotmail.com>
2022-05-26 20:03:22 +0300

    FACTOR FINDER

--- main.py
+++ main.py
@@ -1,70 +1,30 @@
-import copy , random , sys ,time
-
-WIDTH = 79
-HEIGHT = 20
-
-ALIVE = '0'
-DEAD = ' '
-
-nextCells = {}
-
-for x in range(WIDTH):
-    for y in range(HEIGHT):
-        if random.randint(0,1) == 0:
-            nextCells[(x,y)] = ALIVE
-        else:
-            nextCells[(x,y)] = DEAD
+import math,sys
 
 
 while True:
-    print('\n' * 50)
-    cells = copy.deepcopy(nextCells)
-
-    for y in range(HEIGHT):
-        for x in range(WIDTH):
-            print(cells[(x,y)],end='')
-        print()
-    print('Press Ctrl-C to quit.')
-
-
-    for x in range(WIDTH):
-        for y in range(HEIGHT):
-            left = (x-1) % WIDTH
-            right =(x+1) % WIDTH
-            above = (y-1) % HEIGHT
-            below = (y+1) % HEIGHT
-
-
-            numNeighbors = 0
-            if cells[(left,above)] == ALIVE:
-                numNeighbors += 1
-            if cells[(x,above)] == ALIVE:
-                numNeighbors +=1
-            if cells[(right,above)] == ALIVE:
-                numNeighbors += 1
-            if cells[(left,y)] == ALIVE:
-                numNeighbors +=1
-            if cells[(right,y)] == ALIVE:
-                numNeighbors +=1
-            if cells[(left, below)] == ALIVE:
-                numNeighbors += 1
-            if cells[(x, below)] == ALIVE:
-                numNeighbors += 1
-            if cells[(right, below)] == ALIVE:
-                numNeighbors += 1
-
-
-            if cells[(x,y)] == ALIVE and(numNeighbors == 2 or numNeighbors==3):
-                nextCells[(x,y)] = ALIVE
-            elif cells[(x,y)] == DEAD and numNeighbors == 3:
-                nextCells[(x,y)] = ALIVE
-            else:
-                nextCells[(x,y)] =DEAD
-
-
-    try:
-        time.sleep(1)
-    except KeyboardInterrupt:
-        print("Conway's Game of Life")
+    print('Enter a positive whole number to factor (or QUIT):')
+    response = input('> ')
+    if response.upper() == 'QUIT':
         sys.exit()
+
+
+    if not (response.isdecimal() and int(response) > 0):
+        continue
+
+    number=int(response)
+
+    factors=[]
+
+    for i in range(1,int(math.sqrt(number))+1):
+        if number % i ==0:
+            factors.append(i)
+            factors.append(number//i)
+
+
+    factors = list(set(factors))
+    factors.sort()
+
+    for i,factor in enumerate(factors):
+        factors[i]=str(factor)
+    print(', '.join(factors))
 
