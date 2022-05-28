03e2fcb26d4e80afd8c18dffe00b1bce7b615816
asiancart <asiancart@hotmail.com>
2022-05-28 19:44:10 +0300

    MAGIC FORTUNE BALL

--- main.py
+++ main.py
@@ -1,12 +1,52 @@
-X_REPEAT = 19
-Y_REPEAT = 12
+import random, time
 
-for y in range(Y_REPEAT):
-    for x in range(X_REPEAT):
-        print(r'/ \_', end='')
+def slowSpacePrint(text, interval=0.1):
+    for character in text:
+        if character == 'I':
+            print('i ', end='', flush=True)
+        else:
+            print(character + ' ', end='', flush=True)
+        time.sleep(interval)
+    print()
     print()
 
 
-    for x in range(X_REPEAT):
-        print(r'\_/ ', end='')
-    print()
+slowSpacePrint('MAGIC FORTUNE BALL')
+time.sleep(0.5)
+slowSpacePrint('ASK ME YOUR YES/NO QUESTION.')
+input('> ')
+
+replies = [
+'LET ME THINK ON THIS...',
+'AN INTERESTING QUESTION...',
+'HMMM... ARE YOU SURE YOU WANT TO KNOW..?',
+'DO YOU THINK SOME THINGS ARE BEST LEFT UNKNOWN..?',
+'I MIGHT TELL YOU, BUT YOU MIGHT NOT LIKE THE ANSWER...',
+'YES... NO... MAYBE... I WILL THINK ON IT...',
+'AND WHAT WILL YOU DO WHEN YOU KNOW THE ANSWER? WE SHALL SEE...',
+'I SHALL CONSULT MY VISIONS...',
+'YOU MAY WANT TO SIT DOWN FOR THIS...',
+    ]
+
+slowSpacePrint(random.choice(replies))
+slowSpacePrint('.' * random.randint(4, 12), 0.7)
+
+
+slowSpacePrint('I HAVE AN ANSWER...', 0.2)
+time.sleep(1)
+
+answers = [
+'YES, FOR SURE',
+'MY ANSWER IS NO',
+'ASK ME LATER',
+'I AM PROGRAMMED TO SAY YES',
+'THE STARS SAY YES, BUT I SAY NO',
+'I DUNNO MAYBE',
+'DOUBTFUL, VERY DOUBTFUL',
+'FOCUS AND ASK ONCE MORE',
+'AFFIRMATIVE',
+'YES, THOUGH YOU MAY NOT LIKE IT',
+'NO, BUT YOU MAY WISH IT WAS SO',
+]
+slowSpacePrint(random.choice(answers), 0.05)
+
