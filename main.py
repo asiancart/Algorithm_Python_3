fb7a83036ccf0aa823bd095c9b060489fdfd7386
asiancart <asiancart@hotmail.com>
2022-06-07 00:11:37 +0300

    prime numbers

--- main.py
+++ main.py
@@ -1,85 +1,45 @@
-import random
+import math , sys
 
-print('''Powerball Lottery, by asiancart.Each powerball lottery ticket costs $2.The jackpot for this game is $1.586 billion! ''')
+def main():
+    print('Prime Numbers, by asiancart')
+    print('Prime numbers are numbers that are only evenly divisible by')
+    print('one and themselves. They are used in a variety of practical')
+    print('applications, but cannot be predicted. They must be')
+    print('calculated one at a time.')
+    print()
 
-while True:
-    print('Enter 5 different numbers from 1 to 69, with spaces between')
-    print('each number. (For example: 5 17 23 42 50)')
-    response = input('> ')
+    while True:
+        print('Enter a number to start searching for primes from:')
+        print('(Try 0 or 1000000000000 (12 zeros) or another number.)')
+        response = input('> ')
+        if response.isdecimal():
+            num = int(response)
+            break
 
-    numbers = response.split()
-    if len(numbers) != 5:
-        print('Please enter 5 numbers, separated by spaces.')
-        continue
+    input('Press Ctrl-C at any time to quit. Press Enter to begin...')
 
 
+    while True:
+        if isPrime(num):
+            print(str(num)+',',end='',flush=True)
+        num = num +1
+
+
+def isPrime(number):
+    if number < 2:
+        return False
+    elif number == 2:
+        return True
+
+    for i in range(2,int(math.sqrt(number))+1):
+        if number % i == 0:
+            return False
+    return True
+
+
+if __name__ =='__main__':
     try:
-        for i in range(5):
-            numbers[i] = int(numbers[i])
-
-    except ValueError:
-        print('Please enter numbers, like 27, 35, or 62.')
-        continue
-
-    for i in range(5):
-        if not (1 <= numbers[i] <= 69):
-            print('The numbers must all be between 1 and 69.')
-            continue
-
-    if len(set(numbers)) != 5:
-        print('You must enter 5 different numbers.')
-        continue
-
-    break
-
-
-while True:
-    print('How many times do you want to play? (Max: 1000000)')
-    response = input('> ')
-
-    try:
-        numPlays = int(response)
-    except ValueError:
-        print('Please enter a number, like 3, 15, or 22000.')
-        continue
-
-    if not (1 <= numPlays <= 1000000):
-        print('You can play between 1 and 1000000 times.')
-        continue
-
-    break
-
-
-price = '$' + str(2*numPlays)
-print('It costs', price, 'to play', numPlays, 'times, but don\'t')
-print('worry. I\'m sure you\'ll win it all back.')
-input('Press Enter to start...')
-
-
-possibleNumbers = list(range(1,70))
-for i in range(numPlays):
-    random.shuffle(possibleNumbers)
-    winningNumbers = possibleNumbers[0:5]
-    winningPowerball = random.randint(1,26)
-
-    print('The winning numbers are: ', end='')
-    allWinningNums = ''
-    for i in range(5):
-        allWinningNums += str(winningNumbers[i]) + ' '
-    allWinningNums += 'and ' + str(winningPowerball)
-    print(allWinningNums.ljust(21), end='')
-
-    if (set(numbers) == set(winningNumbers) and powerball == winningPowerball):
-        print()
-        print('You have won the Powerball Lottery! Congratulations,')
-        print('you would be a billionaire if this was real!')
-        break
-
-    else:
-        print(' You lost.')
-
-print('You have wasted', price)
-print('Thanks for playing!')
-
-
+        main()
+    except KeyboardInterrupt:
+        sys.exit()
 
