a628557391c90289ef7a2def6f9d89b3b3f7c5de
asiancart <asiancart@hotmail.com>
2022-06-07 21:29:49 +0300

    rock paper scissors

--- main.py
+++ main.py
@@ -1,46 +1,71 @@
-import random , time
+import random , time , sys
 
-BAR = chr(9608)
+print('''Rock, Paper, Scissors, by asiancart''')
 
-def main():
-    print('Progress Bar Simulation, by asiancart')
-    bytesDownloaded= 0
-    downloadSize = 4096
-    while bytesDownloaded < downloadSize:
-        bytesDownloaded += random.randint(0,100)
+wins = 0
+losses = 0
+ties = 0
 
-        barStr = getProgressBar(bytesDownloaded,downloadSize)
+while True:
+    while True:
+        print('{} Wins , {} Losses, {} Ties'.format(wins,losses,ties))
+        print('Enter your move: R P S or (Q)uit')
+        playerMove = input('> ').upper()
+        if playerMove == 'Q':
+            print('Thanks for playing')
+            sys.exit()
 
-        print(barStr,end='',flush=True)
-        time.sleep(0.2)
+        if playerMove == 'R' or playerMove == 'P' or playerMove == 'S':
+            break
+        else:
+            print('Type one of  R, P, S, Q.')
 
-        print('\b' * len(barStr),end='',flush=True)
+    if playerMove == 'R':
+        print('ROCK versus...')
+        playerMove = 'ROCK'
+    elif playerMove == 'P':
+        print('PAPER versus...')
+        playerMove = 'PAPER'
+    elif playerMove == 'S':
+        print('SCISSORS versus...')
+        playerMove = 'SCISSORS'
 
+    time.sleep(0.5)
+    print('1...')
+    time.sleep(0.25)
+    print('2...')
+    time.sleep(0.25)
+    print('3...')
+    time.sleep(0.25)
 
-def getProgressBar(progress,total,barWidth=40):
+    randomNumber = random.randint(1,3)
+    if randomNumber == 1:
+        computerMove = 'ROCK'
+    elif randomNumber == 2:
+        computerMove = 'PAPER'
+    elif randomNumber == 3:
+        computerMove = 'SCISSORS'
+    print(computerMove)
+    time.sleep(0.5)
 
-    progressBar = ''
-    progressBar += '['
-
-    if progress > total:
-        progress = total
-    if progress< total:
-        progress = 0
-
-
-    numberOfBars = int((progress/total) *barWidth)
-
-    progressBar += BAR * numberOfBars
-    progressBar += ' ' * (barWidth-numberOfBars)
-    progressBar += ']'
-
-    percentComplete = round(progress/total*100,1)
-    progressBar = ' ' + str(percentComplete)+ '%'
-
-    progressBar += ' ' +str(progress)+ '/' +str(total)
-
-    return progressBar
-
-
-if __name__ == '__main__':
-    main()
+    if playerMove == computerMove:
+        print('its a tie!')
+        ties = ties + 1
+    elif playerMove == 'ROCK' and computerMove == 'SCISSORS':
+        print('You win')
+        wins = wins+1
+    elif  playerMove == 'PAPER' and computerMove == 'ROCK':
+        print('You win')
+        wins = wins + 1
+    elif playerMove == 'SCISSORS' and computerMove == 'PAPER':
+        print('You win')
+        wins = wins+1
+    elif playerMove == 'ROCK' and computerMove == 'PAPER':
+        print('You lost')
+        losses = losses +1
+    elif playerMove == 'PAPER' and computerMove == 'SCISSORS':
+        print('You lost')
+        losses = losses + 1
+    elif playerMove == 'PAPER' and computerMove == 'SCISSORS':
+        print('You lost')
+        losses = losses + 1
