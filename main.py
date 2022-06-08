7288a885b159e52e83cfcce05cf5afd43de87019
asiancart <asiancart@hotmail.com>
2022-06-08 11:15:25 +0300

    rock paper scissors always win version

--- main.py
+++ main.py
@@ -1,34 +1,31 @@
-import random , time , sys
+import time , sys
 
-print('''Rock, Paper, Scissors, by asiancart''')
+print('''Rock, Paper, Scissors, by asiancart ''')
+
 
 wins = 0
-losses = 0
-ties = 0
 
 while True:
     while True:
-        print('{} Wins , {} Losses, {} Ties'.format(wins,losses,ties))
-        print('Enter your move: R P S or (Q)uit')
+        print('{} Wins, 0 Losses, 0 Ties'.format(wins))
+        print('Enter your move R , P , S , (Q)uit')
         playerMove = input('> ').upper()
         if playerMove == 'Q':
             print('Thanks for playing')
             sys.exit()
 
-        if playerMove == 'R' or playerMove == 'P' or playerMove == 'S':
+        if playerMove== 'R' or playerMove=='P' or playerMove=='S':
             break
         else:
-            print('Type one of  R, P, S, Q.')
+            print('Type P R S or Q.')
 
     if playerMove == 'R':
         print('ROCK versus...')
-        playerMove = 'ROCK'
     elif playerMove == 'P':
         print('PAPER versus...')
-        playerMove = 'PAPER'
     elif playerMove == 'S':
         print('SCISSORS versus...')
-        playerMove = 'SCISSORS'
+
 
     time.sleep(0.5)
     print('1...')
@@ -38,34 +35,15 @@ while True:
     print('3...')
     time.sleep(0.25)
 
-    randomNumber = random.randint(1,3)
-    if randomNumber == 1:
-        computerMove = 'ROCK'
-    elif randomNumber == 2:
-        computerMove = 'PAPER'
-    elif randomNumber == 3:
-        computerMove = 'SCISSORS'
-    print(computerMove)
+
+    if playerMove == 'R':
+        print('Scissors')
+    elif playerMove == 'P':
+        print('Rock')
+    elif playerMove == 'S':
+        print('Paper')
+
     time.sleep(0.5)
 
-    if playerMove == computerMove:
-        print('its a tie!')
-        ties = ties + 1
-    elif playerMove == 'ROCK' and computerMove == 'SCISSORS':
-        print('You win')
-        wins = wins+1
-    elif  playerMove == 'PAPER' and computerMove == 'ROCK':
-        print('You win')
-        wins = wins + 1
-    elif playerMove == 'SCISSORS' and computerMove == 'PAPER':
-        print('You win')
-        wins = wins+1
-    elif playerMove == 'ROCK' and computerMove == 'PAPER':
-        print('You lost')
-        losses = losses +1
-    elif playerMove == 'PAPER' and computerMove == 'SCISSORS':
-        print('You lost')
-        losses = losses + 1
-    elif playerMove == 'PAPER' and computerMove == 'SCISSORS':
-        print('You lost')
-        losses = losses + 1
+    print('You win')
+    wins = wins +1
