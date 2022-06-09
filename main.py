11f18cb2748eaf0ab129823a90a23adb7ea7f26a
asiancart <asiancart@hotmail.com>
2022-06-09 23:49:14 +0300

    SLIDING TILE PUZZLE

--- main.py
+++ main.py
@@ -1,28 +1,120 @@
-import math, shutil, sys, time
+import random, sys
 
-WIDTH, HEIGHT = shutil.get_terminal_size()
-
-WIDTH -=1
-
-print('Sine message by asiancart')
-print('Press Ctrl-C to Quit.')
-print()
-print('What message do you want to display? (Max', WIDTH // 2, 'chars.)')
-while True:
-    message = input('> ')
-    if 1 <= len(message) <= (WIDTH//2):
-        break
-    print('Message must be 1 to',WIDTH//2,'characters long.')
+BLANK = ' '
 
 
-step = 0.0
-multiplier = (WIDTH-len(message)) / 2
-try:
+def main():
+    print('''Sliding Tile Puzzle, by asiancart''')
+    input('Press Enter to begin...')
+
+    gameBoard = getNewPuzzle()
+
     while True:
-        sinOfStep = math.sin(step)
-        padding = ' ' * int((sinOfStep + 1) * multiplier)
-        print(padding + message)
-        time.sleep(0.1)
-        step += 0.25
-except KeyboardInterrupt:
-    sys.exit()
+        displayBoard(gameBoard)
+        playerMove = askForPlayerMove(gameBoard)
+        makeMove(gameBoard,playerMove)
+
+        if gameBoard == getNewBoard():
+            print('You won')
+            sys.exit()
+
+
+def getNewBoard():
+    return [['1 ', '5 ', '9 ', '13'], ['2 ', '6 ', '10', '14'],['3 ', '7 ', '11', '15'], ['4 ', '8 ', '12', BLANK]]
+
+
+def displayBoard(board):
+    labels = [board[0][0], board[1][0], board[2][0], board[3][0],
+              board[0][1], board[1][1], board[2][1], board[3][1],
+              board[0][2], board[1][2], board[2][2], board[3][2],
+              board[0][3], board[1][3], board[2][3], board[3][3]]
+
+    boardToDraw = """   
+                        +------+------+------+------+
+                        |      |      |      |      |
+                        |  {}  |  {}  |  {}  |  {}  |
+                        |      |      |      |      |
+                        +------+------+------+------+
+                        |      |      |      |      |
+                        |  {}  |  {}  |  {}  |  {}  |
+                        |      |      |      |      |
+                        +------+------+------+------+
+                        |      |      |      |      |
+                        |  {}  |  {}  |  {}  |  {}  |
+                        |      |      |      |      |
+                        +------+------+------+------+
+                        |      |      |      |      |
+                        |  {}  |  {}  |  {}  |  {}  |
+                        |      |      |      |      |
+                        +------+------+------+------+ """.format(*labels)
+    print(boardToDraw)
+
+
+def findBlankSpace(board):
+    for x in range(4):
+        for y in range(4):
+            if board[x][y] ==' ':
+                return (x,y)
+
+
+def askForPlayerMove(board):
+    blankx, blanky = findBlankSpace(board)
+
+    w = 'W' if blanky != 3 else ' '
+    a = 'A' if blankx != 3 else ' '
+    s = 'S' if blanky != 0 else ' '
+    d = 'D' if blankx != 0 else ' '
+
+    while True:
+        print('                      ({})'.format(w))
+        print('Enter WASD (or QUIT): ({}) ({}) ({})'.format(a, s, d))
+
+
+        response = input('> ').upper()
+        if response =='QUIT':
+            sys.exit()
+        if response in (w+a+s+d).replace(' ',''):
+            return response
+
+
+def makeMove(board,move):
+    bx,by= findBlankSpace(board)
+
+    if move == 'W':
+        board[bx][by], board[bx][by+1] = board[bx][by+1], board[bx][by]
+    elif move == 'A':
+        board[bx][by], board[bx + 1][by] = board[bx + 1][by], board[bx][by]
+    elif move == 'S':
+        board[bx][by], board[bx][by - 1] = board[bx][by - 1], board[bx][by]
+    elif move == 'D':
+        board[bx][by], board[bx - 1][by] = board[bx - 1][by], board[bx][by]
+
+
+def makeRandomMove(board):
+    blankx,blanky = findBlankSpace(board)
+    validMoves = []
+    if blanky != 3:
+        validMoves.append('W')
+    if blankx != 3:
+        validMoves.append('A')
+    if blanky != 0:
+        validMoves.append('S')
+    if blankx != 0:
+        validMoves.append('D')
+
+
+    makeMove(board,random.choice(validMoves))
+
+
+def getNewPuzzle(moves=200):
+    board = getNewBoard()
+
+    for i in range(moves):
+        makeRandomMove(board)
+    return board
+
+
+if __name__ == '__main__':
+    main()
+
+
