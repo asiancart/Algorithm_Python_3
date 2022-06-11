903a22aee23f8416ed1b6d7e0cc10a9bb69c9dd2
asiancart <asiancart@hotmail.com>
2022-06-11 14:55:54 +0300

    spongecase

--- main.py
+++ main.py
@@ -1,126 +1,47 @@
-NUMBER_OF_DIGITS = 10
+import random
+
+
+try:
+    import pyperclip
+except ImportError:
+    pass
+
 
 def main():
-    print('Soroban - The Japanese Abacus')
-    print('By asiancart')
+    """Run the Spongetext program."""
+    print('''sPoNgEcAsE, bY aSianCarT''')
+
+    spongetext = englishToSpongecase(input('> '))
     print()
+    print(spongetext)
 
-    abacusNumber = 0
+    try:
+        pyperclip.copy(spongetext)
+        print('(cOpIed SpOnGeTexT to ClIpbOaRd.)')
+    except:
+        pass
 
-    while True:
-        displayAbacus(abacusNumber)
-        displayControls()
 
-        commands = input('> ')
-        if commands == 'quit':
-            break
-        elif commands.isdecimal():
-            abacusNumber = int(commands)
+def englishToSpongecase(message):
+    """Return the spongetext form of the given string."""
+    spongetext = ''
+    useUpper = False
+
+    for character in message:
+        if not character.isalpha():
+            spongetext += character
+            continue
+
+        if useUpper:
+            spongetext += character.upper()
         else:
-            for letter in commands:
-                if letter == 'q':
-                    abacusNumber += 1000000000
-                elif letter == 'a':
-                    abacusNumber -= 1000000000
-                elif letter == 'w':
-                    abacusNumber += 100000000
-                elif letter == 's':
-                    abacusNumber -= 100000000
-                elif letter == 'e':
-                    abacusNumber += 10000000
-                elif letter == 'd':
-                    abacusNumber -= 10000000
-                elif letter == 'r':
-                    abacusNumber += 1000000
-                elif letter == 'f':
-                    abacusNumber -= 1000000
-                elif letter == 't':
-                    abacusNumber += 100000
-                elif letter == 'g':
-                    abacusNumber -= 100000
-                elif letter == 'y':
-                    abacusNumber += 10000
-                elif letter == 'h':
-                    abacusNumber -= 10000
-                elif letter == 'u':
-                    abacusNumber += 1000
-                elif letter == 'j':
-                    abacusNumber -= 1000
-                elif letter == 'i':
-                    abacusNumber += 100
-                elif letter == 'k':
-                    abacusNumber -= 100
-                elif letter == 'o':
-                    abacusNumber += 10
-                elif letter == 'l':
-                    abacusNumber -= 10
-                elif letter == 'p':
-                    abacusNumber += 1
-                elif letter == ';':
-                    abacusNumber -= 1
+            spongetext += character.lower()
 
-        if abacusNumber < 0:
-            abacusNumber = 0
+        if random.randint(1,100)<= 90:
+            useUpper = not useUpper
 
-        if abacusNumber >  9999999999:
-            abacusNumber = 9999999999
+    return spongetext
 
-def displayAbacus(number):
-    numberList = list(str(number).zfill((NUMBER_OF_DIGITS)))
-
-    hasBead = []
-
-    for i in range(NUMBER_OF_DIGITS):
-        hasBead.append(numberList[i] in '01234')
-
-    for i in range(NUMBER_OF_DIGITS):
-        hasBead.append(numberList[i] in '56789')
-
-    for i in range(NUMBER_OF_DIGITS):
-        hasBead.append(numberList[i] in '0123456789')
-
-    for i in range(NUMBER_OF_DIGITS):
-        hasBead.append(numberList[i] in '234789')
-
-    for i in range(NUMBER_OF_DIGITS):
-        hasBead.append(numberList[i] in '034589')
-
-    for i in range(NUMBER_OF_DIGITS):
-        hasBead.append(numberList[i] in '014569')
-
-    for i in range(NUMBER_OF_DIGITS):
-        hasBead.append(numberList[i] in '012567')
-
-    for i in range(NUMBER_OF_DIGITS):
-        hasBead.append(numberList[i] in '01235678')
-
-    abacusChar = []
-    for i, beadPresent in enumerate(hasBead):
-        if beadPresent:
-            abacusChar.append('0')
-        else:
-            abacusChar.append('|')
-
-    chars = abacusChar + numberList
-    print(''' 
-    +================================+
-    I  {}  {}  {}  {}  {}  {}  {}  {}  {}  {}  I
-    I  |  |  |  |  |  |  |  |  |  |  I
-    I  {}  {}  {}  {}  {}  {}  {}  {}  {}  {}  I
-    +================================+
-    I  {}  {}  {}  {}  {}  {}  {}  {}  {}  {}  I
-    I  {}  {}  {}  {}  {}  {}  {}  {}  {}  {}  I
-    I  {}  {}  {}  {}  {}  {}  {}  {}  {}  {}  I
-    I  {}  {}  {}  {}  {}  {}  {}  {}  {}  {}  I
-    I  {}  {}  {}  {}  {}  {}  {}  {}  {}  {}  I
-    I  {}  {}  {}  {}  {}  {}  {}  {}  {}  {}  I
-    +=={}=={}=={}=={}=={}=={}=={}=={}=={}=={}==+'''.format(*chars))
-
-
-def displayControls():
-    print(' +q w e r t y u i o p')
-    print(' -a s d f g h j k l ;')
-    print('(Enter a number, "quit", or a stream of up/down letters.)')
 
 if __name__ == '__main__':
     main()
