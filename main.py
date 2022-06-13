d44e94f0954a4bea07649cdbc15b7d5722229c47
asiancart <asiancart@hotmail.com>
2022-06-13 22:37:10 +0300

    snake game pygame

--- main.py
+++ main.py
@@ -1,120 +1,126 @@
-import sys, random , time
+import pygame
+import time
+import random
 
-try:
-    import bext
-except ImportError:
-    print('This program requires the bext module')
-    sys.exit()
+pygame.init()
 
+white = (255, 255, 255)
+yellow = (255, 255, 102)
+black = (0, 0, 0)
+red = (213, 50, 80)
+green = (0, 255, 0)
+blue = (50, 153, 213)
 
-WIDTH , HEIGHT = bext.size()
+dis_width = 600
+dis_height = 400
 
-WIDTH -= 1
-NUMBER_OF_LOGOS = 5
-PAUSE_AMOUNT = 0.2
+dis = pygame.display.set_mode((dis_width, dis_height))
+pygame.display.set_caption('Snake Game by asiancart')
 
-COLORS = ['red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
+clock = pygame.time.Clock()
 
-UP_RIGHT = 'ur'
-UP_LEFT = 'ul'
-DOWN_RIGHT = 'dr'
-DOWN_LEFT = 'dl'
-DIRECTIONS = (UP_RIGHT, UP_LEFT, DOWN_RIGHT, DOWN_LEFT)
+snake_block = 10
+snake_speed = 15
 
-COLOR = 'color'
-X = 'x'
-Y = 'y'
-DIR = 'direction'
-
-def main():
-    bext.clear()
-
-    logos = []
-    for i in range(NUMBER_OF_LOGOS):
-        logos.append({COLOR: random.choice(COLORS),
-                      X: random.randint(1, WIDTH - 4),
-                      Y: random.randint(1, HEIGHT - 4),
-                      DIR: random.choice(DIRECTIONS)})
-
-        if logos[-1][X] % 2 == 1:
-            logos[-1][X] -= 1
-
-    cornerBounces = 0
-    while True:
-        for logo in logos:
-            bext.goto(logo[X], logo[Y])
-            print('   ', end='')
-
-            originalDirection = logo[DIR]
-
-            if logo[X] == 0 and logo[Y] == 0:
-                logo[DIR] = DOWN_RIGHT
-                cornerBounces += 1
-            elif logo[X] == 0 and logo[Y] == HEIGHT - 1:
-                logo[DIR] = UP_RIGHT
-                cornerBounces += 1
-            elif logo[X] == WIDTH - 3 and logo[Y] == 0:
-                logo[DIR] = DOWN_LEFT
-                cornerBounces += 1
-            elif logo[X] == WIDTH - 3 and logo[Y] == HEIGHT - 1:
-                logo[DIR] = UP_LEFT
-                cornerBounces += 1
-
-            elif logo[X] == 0 and logo[DIR] == UP_LEFT:
-                logo[DIR] = UP_RIGHT
-            elif logo[X] == 0 and logo[DIR] == DOWN_LEFT:
-                logo[DIR] = DOWN_RIGHT
-
-            elif logo[X] == WIDTH - 3 and logo[DIR] == UP_RIGHT:
-                logo[DIR] = UP_LEFT
-            elif logo[X] == WIDTH - 3 and logo[DIR] == DOWN_RIGHT:
-                logo[DIR] = DOWN_LEFT
-            elif logo[Y] == 0 and logo[DIR] == UP_LEFT:
-                logo[DIR] = DOWN_LEFT
-            elif logo[Y] == 0 and logo[DIR] == UP_RIGHT:
-                logo[DIR] = DOWN_RIGHT
-            elif logo[Y] == HEIGHT - 1 and logo[DIR] == DOWN_LEFT:
-                logo[DIR] = UP_LEFT
-            elif logo[Y] == HEIGHT - 1 and logo[DIR] == DOWN_RIGHT:
-                logo[DIR] = UP_RIGHT
-
-            if logo[DIR] != originalDirection:
-                logo[COLOR] = random.choice(COLORS)
-
-            if logo[DIR] == UP_RIGHT:
-                logo[X] += 2
-                logo[Y] -= 1
-            elif logo[DIR] == UP_LEFT:
-                logo[X] -= 2
-                logo[Y] -= 1
-            elif logo[DIR] == DOWN_RIGHT:
-                logo[X] += 2
-                logo[Y] += 1
-            elif logo[DIR] == DOWN_LEFT:
-                logo[X] -= 2
-                logo[Y] += 1
-
-        bext.goto(5, 0)
-        bext.fg('white')
-        print('Corner bounces:', cornerBounces, end='')
-
-        for logo in logos:
-            bext.goto(logo[X], logo[Y])
-            bext.fg(logo[COLOR])
-            print('DVD', end='')
-
-        bext.goto(0, 0)
-
-        sys.stdout.flush()
-        time.sleep(PAUSE_AMOUNT)
-
-if __name__ == '__main__':
-    try:
-        main()
-    except KeyboardInterrupt:
-        print()
-        sys.exit()
+font_style = pygame.font.SysFont("bahnschrift", 25)
+score_font = pygame.font.SysFont("comicsansms", 35)
 
 
+def Your_score(score):
+    value = score_font.render("Your Score: " + str(score), True, yellow)
+    dis.blit(value, [0, 0])
 
 
+def our_snake(snake_block, snake_list):
+    for x in snake_list:
+        pygame.draw.rect(dis, black, [x[0], x[1], snake_block, snake_block])
+
+
+def message(msg, color):
+    mesg = font_style.render(msg, True, color)
+    dis.blit(mesg, [dis_width / 6, dis_height / 3])
+
+
+def gameLoop():
+    game_over = False
+    game_close = False
+
+    x1 = dis_width / 2
+    y1 = dis_height / 2
+
+    x1_change = 0
+    y1_change = 0
+
+    snake_List = []
+    Length_of_snake = 1
+
+    foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+    foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+
+    while not game_over:
+
+        while game_close == True:
+            dis.fill(blue)
+            message("You Lost! Press C-Play Again or Q-Quit", red)
+            Your_score(Length_of_snake - 1)
+            pygame.display.update()
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_q:
+                        game_over = True
+                        game_close = False
+                    if event.key == pygame.K_c:
+                        gameLoop()
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                game_over = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    x1_change = -snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_RIGHT:
+                    x1_change = snake_block
+                    y1_change = 0
+                elif event.key == pygame.K_UP:
+                    y1_change = -snake_block
+                    x1_change = 0
+                elif event.key == pygame.K_DOWN:
+                    y1_change = snake_block
+                    x1_change = 0
+
+        if x1 >= dis_width or x1 < 0 or y1 >= dis_height or y1 < 0:
+            game_close = True
+        x1 += x1_change
+        y1 += y1_change
+        dis.fill(blue)
+        pygame.draw.rect(dis, green, [foodx, foody, snake_block, snake_block])
+        snake_Head = []
+        snake_Head.append(x1)
+        snake_Head.append(y1)
+        snake_List.append(snake_Head)
+        if len(snake_List) > Length_of_snake:
+            del snake_List[0]
+
+        for x in snake_List[:-1]:
+            if x == snake_Head:
+                game_close = True
+
+        our_snake(snake_block, snake_List)
+        Your_score(Length_of_snake - 1)
+
+        pygame.display.update()
+
+        if x1 == foodx and y1 == foody:
+            foodx = round(random.randrange(0, dis_width - snake_block) / 10.0) * 10.0
+            foody = round(random.randrange(0, dis_height - snake_block) / 10.0) * 10.0
+            Length_of_snake += 1
+
+        clock.tick(snake_speed)
+
+    pygame.quit()
+    quit()
+
+
+gameLoop()
