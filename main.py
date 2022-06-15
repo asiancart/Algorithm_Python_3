723c2e82934175cf7e5d5d0193bb9b5793e6ba81
asiancart <asiancart@hotmail.com>
2022-06-15 12:52:37 +0300

    tree drawing

--- main.py
+++ main.py
@@ -1,153 +1,57 @@
-import random
+import random, time
 
-print('''Carrot in a Box, by asiancart. 
-This is a bluffing game for two human players. Each player has a box.
-One box has a carrot in it. To win, you must have the box with the carrot in it.
-The first player looks into their box (the second player must close
-their eyes during this). The first player then says "There is a carrot
-in my box" or "There is not a carrot in my box". The second player then
-gets to decide if they want to swap boxes or not.
-''')
+class Tree():
 
-input("Press enter to begin..")
+    def __init__(self):
+        self.start()
 
-p1Name = input("Human player 1 , enter your name: ")
-p2Name = input("Human player 2 , enter your name: ")
-playerNames = p1Name[:11].center(11)+  "        "+p2Name[:11].center(11)
+    def start(self):
+        try:
+            print("Tree drawing program")
+            age = int(input("enter age of tree: "))
 
-print('''HERE ARE TWO BOXES
-  ___________  ___________
- /         /| /          /|
-+---------+ | +---------+ |
-|   RED   | | |   GOLD  | |
-|   BOX   | / |   BOX   | /
-+---------+/  +---------+/
+            kind = random.randrange(2)
 
-''')
+            if kind == 1:
 
-print()
-print(playerNames)
-print()
-print(p1Name+", you have a RED box in front of you")
-print(p2Name+", you have a GOLD box in front of you")
-print()
-print(p1Name+", you will get to look into your box.")
-print(p2Name.upper()+", close your eyes and do not look please.")
-input("When "+p2Name+" has closed their eyes press Enter...")
-print()
+                leaf=round((age/3)*2)
+                for s in range(0,leaf):
+                    print(" "*(leaf-s),"*"*((s*2)+1)," "*(leaf-s))
 
-print(p1Name+ " here is the inside of your box:")
+                body= round(age/3)
 
-if random.randint(1,2) == 1:
-    carrotInFirstBox = True
-else:
-    carrotInFirstBox = False
+                for i in range(body):
+                    print(" "*(body*2-1),"||"," "*(body*2-1))
 
-if carrotInFirstBox:
-    print('''
-    ___VV____
-    |  VV   |
-    |  VV   |
-    |__||___|   _______
-    /  ||  /|  /     /|
-    +-----+ | +-----+ |
-    | RED | | | GOLD| |
-    | BOX | / | BOX | /
-    +-----+/  +-----+/       
-    (carrot!)''')
-    print(playerNames)
-else:
+            else:
+                leaves1=[]
+                leaves2=[]
+                leaves3=[]
+                leaf=(age//3)*2
+                for s in range(leaf,0,-1):
+                    leaves2.append(" "*s+"*"*(leaf-s)+ "*"*(leaf-s)+" "*s)
 
-    print('''
-    _________
-    |       |
-    |       |
-    |_______|  _________
-    /      /| /       /|
-    +-------+ | +----+ |
-    | RED | | | GOLD | |
-    | BOX | / | BOX  | /
-    +-----+/  +------+/
-    (no carrot!)''')
-    print(playerNames)
+                for s in range(1,leaf):
+                    leaves1.append(" "*s+"*"*(leaf-s)+"*"*(leaf-s)+" "*s)
 
-input("Press Enter to continue...")
+                leaves3 = leaves2+leaves1
+                print(*leaves3,sep="\n")
 
-print("\n"*100)
-print(p1Name+", tell "+p2Name+" to open their eyes")
-input("Press Enter to continue...")
-
-print()
-print(p1Name + ', say one of the following sentences to ' + p2Name + '.')
-print(' 1) There is a carrot in my box.')
-print(' 2) There is not a carrot in my box.')
-print()
-input('Then press Enter to continue...')
+                body=age//3
 
 
-print()
-print(p2Name + ', do you want to swap boxes with ' + p1Name + '? YES/NO')
-while True:
-    response = input("> ").upper()
-    if not (response.startswith("Y") or response.startswith("Y")):
-        print(p2Name + ', please enter "YES" or "NO".')
-    else:
-        break
+                for i in range(body):
+                    print(" "*(body*2-2),"||"," "*(body*2-1))
 
+        except:
+            print("There is an error")
 
-firstBox = 'RED '
-secondBox = 'GOLD'
+if __name__ == "__main__":
+    tree=Tree()
 
-if response.startswith('Y'):
-    carrotInFirstBox = not carrotInFirstBox
-    firstBox, secondBox = secondBox, firstBox
-
-
-print('''HERE ARE THE TWO BOXES:
-     __________  __________
-    /        /| /        /|
-    +-------+ | +-------+ |
-    |   {}  | | |  {} | |
-    |  BOX  | / |  BOX  | /
-    +-------+/  +-------+/ '''.format(firstBox,secondBox))
-print(playerNames)
-
-input('Press Enter to reveal the winner...')
-print()
-
-if carrotInFirstBox:
-    print('''
-    ___VV____
-    |  VV   |
-    |  VV   |
-    |__||___|   _______
-    /  ||  /|  /     /|
-    +-----+ | +-----+ |
-    | {} | | | {}| |
-    | BOX | / | BOX | /
-    +-----+/  +-----+/            
-    '''.format(firstBox,secondBox))
-
-else:
-    print('''
-     _______ ___VV___
-    |      | |  VV  |
-    |      | |  VV  |
-    |______| |__||__|
-     / /| / || /|
-    +-----+ | +-----+ |
-    |  {}  |   | | {} | |
-    |  BOX  | / | BOX | /
-    +-------+/  +-------+/            
-    '''.format(firstBox,secondBox))
-
-print(playerNames)
-
-if carrotInFirstBox:
-    print(p1Name+" is the winner")
-    print(p2Name+" is the winner")
-
-print("Thanks for playing.")
-
-
-
+    print("")
+    count=20
+    while count:
+        print("\r Program is closing. Last {} seconds...".format(count),end="")
+        count -= 1
+        time.sleep(1)
