96adc0a3ae25652ba9fa8bf4defd73a848e8a534
asiancart <asiancart@hotmail.com>
2022-06-16 13:07:06 +0300

    alarm clock with python

--- main.py
+++ main.py
@@ -1,18 +1,22 @@
-import requests
-import bs4
-link =input("Enter the url: ")
-page =requests.get(link)
-print(page.text)
+from datetime import datetime
+from playsound import playsound
 
-#print the text in a readable form..
-soup =bs4.BeautifulSoup(page.text,'html.parser')
-text_format =soup.prettify()
-print(text_format)
-
-#print the list of image tag....
-image_list =soup.find_all('img')
-print(image_list)
-
-#print the no of image tag.....
-count_the_image =len(image_list)
-print(count_the_image)
+alarm_time = input('Enter the time of alarm to be set: HH:MM:SS\n')
+alarm_hour = alarm_time[0:2]
+alarm_minute = alarm_time[3:5]
+alarm_seconds = alarm_time[6:8]
+alarm_period = alarm_time[9:11].upper()
+print('Setting up alarm...')
+while True:
+    now = datetime.now()
+    current_hour=now.strftime("%I")
+    current_minute = now.strftime("%M")
+    current_seconds = now.strftime("%S")
+    current_period = now.strftime("%p")
+    if (alarm_period==current_period):
+        if(alarm_hour==current_hour):
+            if(alarm_minute==current_minute):
+                if(alarm_seconds==current_seconds):
+                    print("Wake up!")
+                    playsound('audio.mp3')
+                    break
